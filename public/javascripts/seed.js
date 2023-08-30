3a888bf8ef3a06856c0607519cf6afce9a90dcde
Ron Dear <rdear4@gmail.com>
2023-08-29 23:13:17 -0400

    Updated seed.js to allow for multiple files at once and to use uuid as image id for later updating

--- public/javascripts/seed.js
+++ public/javascripts/seed.js
@@ -2,45 +2,73 @@ var express = require('express');
 var router = express.Router();
 const fs = require('fs');
 var path = require('path');
+const { v4: uuidv4 } = require('uuid');
 
-const TEST_FILENAME = "dear-emily.json"
+const TEST_FILES = [
+    {
+        name: "Ron Dear",
+        filename: "dear-ron"
+    },
+    {
+        name: "Thomas Dear",
+        filename: "dear-thomas"
+    },
+    {
+        name: "Emily Dear",
+        filename: "dear-emily"
+    }
+]
+
+
 
 //Create the test file
 
-let testFileData = {
-    "name": "Ron Dear",
-    "images": [
-        {
-            "dimensions": [32, 32],
-            "pixelInfo": []
-        }
-    ],
-    "updateLog": [
-
+const createFile = (name, filename) => {
+    
+    let testFileData = {
+        
+        "name": name,
+        "images": [
+            {
+                "id": uuidv4(),
+                "name": "Image - 1",
+                "dimensions": [32, 32],
+                "pixelInfo": []
+            }
+        ],
+        "updateLog": [
+    
+        ]
+    }
+    
+    // Create the first test image data - alternating RGBCyan color sequence
+    
+    let dummyDataOptions = [
+        [255, 0, 0, 255],
+        [0, 255, 0, 255],
+        [0, 0, 255, 255],
+        [0, 255, 255, 255],
     ]
+    
+    for (let i = 0; i < 32 * 32; i++) {
+    
+        testFileData.images[0].pixelInfo[i] = dummyDataOptions[i % 4]
+    
+    }
+    
+    testFileData.updateLog.push(new Date().getTime())
+    
+    try {
+        fs.writeFileSync(`${__dirname}/saved/${filename}.json`, JSON.stringify(testFileData))
+        return true
+      } catch (err) {
+        console.log(err)
+        return false
+      }
+
 }
 
-// Create the first test image data - alternating RGBCyan color sequence
-
-let dummyDataOptions = [
-    [255, 0, 0, 255],
-    [0, 255, 0, 255],
-    [0, 0, 255, 255],
-    [0, 255, 255, 255],
-]
-
-for (let i = 0; i < 32 * 32; i++) {
-
-    testFileData.images[0].pixelInfo[i] = dummyDataOptions[i % 4]
-
+for (let file of TEST_FILES) {
+    
+    createFile(file.name, file.filename)
 }
-
-testFileData.updateLog.push(new Date().getTime())
-
-try {
-    fs.writeFileSync(`${__dirname}/saved/${TEST_FILENAME}`, JSON.stringify(testFileData))
-    return true
-  } catch (err) {
-    console.log(err)
-    return false
-  }
